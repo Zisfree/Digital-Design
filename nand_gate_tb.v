88e35cb1bc9a0b301fd53a04f3bd80552373601a
shame0x2 <ajoshi2134@gmail.com>
2026-09-04 19:21:33 +0530

    Create nand_gate_tb.v

---
+++ nand_gate_tb.v
@@ -0,0 +1,20 @@
+module nand_gate_tb;
+reg A, B;
+wire Y;
+
+nand_gate uut(.A(A), .B(B), .Y(Y));
+
+initial begin
+ $dumpfile("nand_gate.vcd");
+ $dumpvars(0, nand_gate_tb);
+ $monitor("Time=%0t, A=%b, B=%b, Y=%b", $time, A, B, Y);
+
+ A = 0; B = 0;
+ #10 A = 1; B = 0;
+ #10 A = 0; B = 1;
+ #10 A = 1; B = 1;
+ #10
+ $finish;
+end
+endmodule
+
